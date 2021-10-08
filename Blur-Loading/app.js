6c63e78cef7d8c2ccbc4203f52e4749bb2aa0399
sigristarisa <arisasigrist.ch@gmail.com>
2021-10-08 15:56:39 +0200

    inital commit

---
+++ Blur-Loading/app.js
@@ -0,0 +1,22 @@
+const loadText = document.getElementById("loading-number");
+const background = document.getElementById("background");
+
+let load = 0;
+
+const blurring = () => {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
+
+const int = setInterval(blurring, 30);
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
