feafeb2149100faa2ad67fbad08dab287b751de2
sigristarisa <arisasigrist.ch@gmail.com>
2021-10-14 11:26:29 +0200

    finalized

--- Browser-Form/app.js
+++ Browser-Form/app.js
@@ -14,14 +14,20 @@ const form = (() => {
     e.preventDefault();
     if (!eMail.value.match(reEMail)) {
       alert("Please type in a valid E-mail address");
+      eMail.style.border = "red solid 2px";
     } else if (!country) {
       alert("Please select your country");
+      country.style.border = "red solid 2px";
     } else if (!zipCode.value) {
       alert("Please type in a valid zip code");
+      zipCode.style.border = "red solid 2px";
     } else if (!password.value.match(rePassword)) {
       alert("Please type a valid password");
+      password.style.border = "red solid 2px";
     } else if (password.value !== confirmation.value) {
       alert("please type the same password");
+      password.style.border = "red solid 2px";
+      confirmation.style.border = "red solid 2px";
     }
   });
 })();
